864a45b138ed927c0251fd3e5677364b2809593d
Léo Guignard <leo.guignard@univ-amu.fr>
2023-06-14 11:31:34 +0200

    First working version

--- src/napari_vectorflow/_reader.py
+++ src/napari_vectorflow/_reader.py
@@ -6,6 +6,9 @@ implement multiple readers or even other plugin contributions. see:
 https://napari.org/stable/plugins/guides.html?#readers
 """
 import numpy as np
+from itertools import product
+from tifffile import imread
+from pathlib import Path
 
 
 def napari_get_reader(path):
@@ -27,9 +30,11 @@ def napari_get_reader(path):
         # if it is a list, it is assumed to be an image stack...
         # so we are only going to look at the first file.
         path = path[0]
+    elif Path(path).is_dir():
+        return reader_function
 
     # if we know we cannot read the file, we immediately return None.
-    if not path.endswith(".npy"):
+    if not path.endswith(".tif") and not path.endswith(".tiff"):
         return None
 
     # otherwise we return the *function* that can read ``path``.
@@ -59,14 +64,69 @@ def reader_function(path):
         default to layer_type=="image" if not provided
     """
     # handle both a string and a list of strings
-    paths = [path] if isinstance(path, str) else path
-    # load all files into array
-    arrays = [np.load(_path) for _path in paths]
-    # stack arrays into single array
-    data = np.squeeze(np.stack(arrays))
+    if isinstance(path, str) and not Path(path).is_dir():
+        paths = [path]
+    elif Path(path).is_dir():
+        paths = [p for p in Path(path).iterdir() if p.suffix in ['.tif', '.tiff']]
+    else:
+        return
+    # # load all files into array
+    # arrays = [imread(_path) for _path in paths]
+    # # stack arrays into single array
+    # data = np.squeeze(np.stack(arrays))
 
     # optional kwargs for the corresponding viewer.add_* method
     add_kwargs = {}
 
-    layer_type = "image"  # optional, default is "image"
-    return [(data, add_kwargs, layer_type)]
+    layer_type = "vectors"  # optional, default is "image"
+    all_vects = []
+    all_lengths = []
+    if 1<len(paths):
+        for t, _path in enumerate(sorted(paths)):
+            data = imread(_path)
+            coord = np.array(list(product(np.arange(data.shape[0]), np.arange(data.shape[1]), np.arange(data.shape[2]))))
+            coord = np.hstack([t+np.zeros(len(coord), dtype=coord.dtype).reshape(-1, 1), coord])
+            vects = np.hstack(
+                    (
+                        coord,
+                        np.zeros((len(coord), 1)),
+                        data[coord[:, 1], coord[:, 2], coord[:, 3], 0].reshape(-1, 1),
+                        data[coord[:, 1], coord[:, 2], coord[:, 3], 1].reshape(-1, 1), 
+                        data[coord[:, 1], coord[:, 2], coord[:, 3], 2].reshape(-1, 1),
+                    )
+                )
+            vects = vects.reshape(-1, 2, 4)
+            all_vects.append(vects)
+            all_lengths.append(np.linalg.norm(vects[:, 1], axis=1))
+        all_vects = np.concatenate(all_vects)
+        all_lengths = np.concatenate(all_lengths)
+    else:
+        data = imread(paths[0])
+        coord = np.array(list(product(np.arange(data.shape[0]), np.arange(data.shape[1]), np.arange(data.shape[2]))))
+        vects = np.hstack(
+                (
+                    coord,
+                    data[coord[:, 0], coord[:, 1], coord[:, 2], 0].reshape(-1, 1),
+                    data[coord[:, 0], coord[:, 1], coord[:, 2], 1].reshape(-1, 1), 
+                    data[coord[:, 0], coord[:, 1], coord[:, 2], 2].reshape(-1, 1),
+                )
+            )
+        all_vects = vects.reshape(-1, 2, 3)
+        all_lengths = np.linalg.norm(all_vects[:, 1], axis=1)
+    features = {
+        "length": all_lengths,
+    }
+    metadata = {
+        # Here we assume that all VF have the same shape
+        "shape": ((len(paths), ) if 1<len(paths) else ()) + data.shape,
+        "init_length": all_lengths,
+        "init_data": all_vects.copy()
+    }
+    add_kwargs = {
+        'edge_width': .1,
+        'features': features, 
+        'metadata': metadata,
+        'edge_colormap': 'viridis',
+        'edge_color': 'length'
+    }
+    return [(all_vects, add_kwargs, layer_type)]
--- src/napari_vectorflow/_widget.py
+++ src/napari_vectorflow/_widget.py
@@ -8,39 +8,126 @@ Replace code below according to your needs.
 """
 from typing import TYPE_CHECKING
 
-from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from magicgui import widgets
+from qtpy.QtWidgets import QVBoxLayout, QComboBox, QWidget
+from qtpy.QtCore import QStringListModel
+import numpy as np
 
 if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
+class VectorFlowWidget(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+    def get_intensity_layers(self, *args):
+        new_choices = [l.name for l in self.viewer.layers if l.as_layer_data_tuple()[-1] == 'image']
+        self.model_choices['intensity'].setStringList(new_choices)
+        return new_choices
+    
+    def get_vectors_layers(self, *args):
+        new_choices = [l.name for l in self.viewer.layers if l.as_layer_data_tuple()[-1] == 'vectors']
+        self.model_choices['vectors'].setStringList(new_choices)
+        return new_choices
+
+    def get_layers(self, *args):
+        new_choices = [l.name for l in self.viewer.layers if l.as_layer_data_tuple()[-1] == 'image']
+        self.model_choices['intensity'].setStringList(new_choices)
+        new_choices = [l.name for l in self.viewer.layers if l.as_layer_data_tuple()[-1] == 'vectors']
+        self.model_choices['vectors'].setStringList(new_choices)
+
+    def threshold_from_image(self):
+        for l in self.viewer.layers:
+            if l.name == self.intensity_cbox.currentText():
+                data_int = l.data
+                layer_int = l
+                if not isinstance(data_int, np.ndarray):
+                    first_tp = np.array(data_int[0]).shape
+                    if not all([first_tp==np.array(data_int[i]).shape for i in range(data_int.shape[0])]):
+                        print('All intensity images have to have the same shape across time.')
+                    else:
+                        data_int = np.array(data_int)
+            if l.name == self.vectors_cbox.currentText():
+                data_vect = l.data
+                layer_vect = l
+        th_val = self.threshold_value.value
+        shape_vect = layer_vect.metadata['shape']
+        shape_int = data_int.shape
+        min_dim = min(len(shape_int), len(shape_vect))
+        shift = len(shape_vect) - 1 - min_dim
+        slicing = tuple(slice(shape_vect[shift+i]) for i in range(min_dim))
+        data_int = data_int[slicing]
+        to_keep = np.where(th_val<data_int)
+        # change = lambda z, y, x: ((shape_vect[1])*(shape_vect[2]))*coord[2] + (shape_vect[2])*coord[1] + coord[0]
+        change = lambda coord: np.sum([(coord[min_dim-1-i]*np.product(shape_vect[-1-i:-1])) for i in range(min_dim)], axis=0, dtype=int)
+        pos_to_keep = change(to_keep)
+        data_subset = layer_vect.metadata['init_data'][pos_to_keep]
+        features_subset = layer_vect.metadata['init_length'][pos_to_keep]
+        layer_vect.data = data_subset
+        layer_vect.features['length'] = features_subset
+        layer_vect.edge_color = 'length'
+        layer_vect.refresh()
+
+
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        # Choice of intensity image and vectorfield
+        self.model_choices = {
+            'intensity': QStringListModel(),
+            'vectors': QStringListModel()
+        }
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        self.get_layers()
+
+        name_int_cbox = widgets.Label(value='Intensity Image:')
+        self.intensity_cbox = QComboBox()
+        self.intensity_cbox.setModel(self.model_choices['intensity'])
+        self.intensity_cbox.native = self.intensity_cbox
+        
+        name_vf_cbox = widgets.Label(value='Vectorfield:')
+        self.vectors_cbox = QComboBox()
+        self.vectors_cbox.setModel(self.model_choices['vectors'])
+        self.vectors_cbox.native = self.vectors_cbox
+        
+        self.data_selection = widgets.Container(
+            widgets=[
+                name_int_cbox,
+                self.intensity_cbox,
+                name_vf_cbox,
+                self.vectors_cbox,
+            ],
+            labels=False
+        )
+
+        self.viewer.layers.events.inserted.connect(self.get_layers)
+        self.viewer.layers.events.removed.connect(self.get_layers)
+
+        # Thresholding from the image
+        label_threshold = widgets.Label(value='Thresholding from\nintensity image')
+        self.threshold_value = widgets.Slider(value=0, min=0, max=255)
+        self.button_threhold = widgets.PushButton(text='Threshold!')
+        self.button_threhold.clicked.connect(self.threshold_from_image)
+        self.threshold_from_image_widget = widgets.Container(
+            widgets=[
+                label_threshold,
+                self.threshold_value,
+                self.button_threhold
+            ],
+            labels=False
+        )
+
+        layout = QVBoxLayout()
+        layout.addStretch(1)
+        layout.setSpacing(0)
+        self.setLayout(layout)
+
+        self.layout().addWidget(self.data_selection.native)
+        self.layout().addWidget(self.threshold_from_image_widget.native)
+
 
     def _on_click(self):
         print("napari has", len(self.viewer.layers), "layers")
-
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
